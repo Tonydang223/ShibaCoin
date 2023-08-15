fccd428eadefa32b86d2350458f6fff2f7369234
Tonydang223 <danghnh@dipro.vn>
2023-08-15 17:30:09 +0700

    next2

--- js/index.js
+++ js/index.js
@@ -3,6 +3,48 @@ $(document).ready(function() {
         $(".navbar-collapse").collapse("hide");
     });
 
+    var button = $("#buttonCt a");
+    var countdown = 10;
+    var timer = setInterval(function() {
+      countdown--;
+      button.text("Open in " + countdown + " seconds");
+      if (countdown <= 0) {
+        clearInterval(timer);
+        button.text("Buy Now!");
+      }
+    }, 1000);
+
+    $("#icScrollTop").on("click", function () {
+        $("html, body").animate({ scrollTop: 0 }, "fast");
+        return false;
+      });
+
+    $(window).on("scroll", function () {
+        const scrollY = window.scrollY;
+        if (scrollY < 200) {
+          $("#icScrollTop").removeClass("ic-scrollTop-show");
+        } else {
+          $("#icScrollTop").addClass("ic-scrollTop-show");
+        }
+    
+        // els.forEach((e) => {
+        //   const top = e.offsetTop - 200;
+        //   const height = e.offsetHeight;
+        //   const id = e.getAttribute("id");
+        //   if (scrollY >= top && top + height >= scrollY) {
+        //     links.forEach((e, i) => {
+        //       e.classList.remove("active");
+        //       document
+        //         .querySelector(`header nav a[href="#${id}"]`)
+        //         ?.classList.add("active");
+        //       if (e.getAttribute("id") === "cvalue") {
+        //         console.log("alo ha");
+        //       }
+        //     });
+        //   }
+        // });
+      });
+
 
     $(".card-header").click(function(){
         // self clicking close
